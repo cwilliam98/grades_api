00eb3165827d3f536d81cf7b4edb986b624396e6
cwilliam98 <cristianwillian174@gmail.com>
2020-09-05 13:46:30 -0300

    novo commit

--- app.js
+++ app.js
@@ -27,10 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin:
-      process.env.PORT_CORS === ''
-        ? process.env.HOST_CORS
-        : `${process.env.HOST_CORS}:${process.env.PORT_CORS}`,
+    origin: 'http://localhost:3000',
   })
 );
 
