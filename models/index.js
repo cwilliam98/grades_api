d2b3c0c4b9dad99721e629448823b797e74e4446
cwilliam98 <cristianwillian174@gmail.com>
2020-09-05 12:46:12 -0300

    novo commit

--- models/index.js
+++ models/index.js
@@ -28,6 +28,14 @@ const gradesSchema = mongoose.Schema({
   },
 });
 
+gradesSchema.method('toJSON', function () {
+  const { _id, ...object } = this.toObject();
+
+  object.id = _id;
+
+  return object;
+});
+
 const gradesModel = mongoose.model('grades', gradesSchema, 'grades');
 
 export { db, gradesModel };
